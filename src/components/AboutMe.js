9b3c1b96691574e09bc1ea054d3f236916d70e89
Tuấn Trần <tuantran280504@gmail.com>
2025-05-24 01:04:06 +0700

    hoan thanh profile

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,7 +10,7 @@ export default function AboutMe({ id }) {
         Mình là <span> Trần Văn Tuấn </span>, hiện đang là sinh viên năm 2 chuyên ngành <span>Công Nghệ Thông Tin</span> tại <span>Trường Cao đẳng Công nghệ Thủ Đức. </span>
         Với niềm đam mê học hỏi và khám phá công nghệ, mình luôn nỗ lực rèn luyện kỹ năng chuyên môn cũng như kỹ năng mềm để chuẩn bị cho hành trình nghề nghiệp phía trước.
         <br /> <br />
-        Mình đặc biệt quan tâm đến <span>lập trình web</span>và đang không ngừng nâng cao kiến thức qua các dự án cá nhân, khóa học trực tuyến và hoạt động ngoại khóa. Website này là nơi mình chia sẻ thông tin cá nhân, sản phẩm đã thực hiện và những điều mình đang học hỏi mỗi ngày.
+        Mình đặc biệt quan tâm đến <span>lập trình web </span>và đang không ngừng nâng cao kiến thức qua các dự án cá nhân, khóa học trực tuyến và hoạt động ngoại khóa. Website này là nơi mình chia sẻ thông tin cá nhân, sản phẩm đã thực hiện và những điều mình đang học hỏi mỗi ngày.
         <br /><br />
         <span>Rất vui được kết nối và học hỏi thêm từ bạn!</span>
       </div>
